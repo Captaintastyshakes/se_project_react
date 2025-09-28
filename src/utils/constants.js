4ab83c7b0328a4037d61a711799aaa28b8bfbb63
Michael Weihmanngit config --global user.name Michael <mikej513@hotmail.com>
2025-09-28 16:38:26 -0600

    some small changes that redirect the api's to go to the remote server- all anticipatory of deploying the front end.

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -9,5 +9,11 @@ const coordinates = { latitude: "53.63344", longitude: "-113.63533" };
 
 export const baseURL = `https://api.openweathermap.org/data/2.5/weather?lat=${coordinates.latitude}&lon=${coordinates.longitude}&units=imperial&appid=${APIKey}`;
 
-export const dbApiUrl = "http://localhost:3001";
+//export const dbApiUrl = "http://localhost:3001";
+
 //export const dbApiUrl = "http://localhost:3000";
+//NEW url endpoint for remote ubuntu server
+export const dbApiUrl =
+  process.env.NODE_ENV === "production"
+    ? "https://api.tastywearlab.jumpingcrab.com"
+    : "http://localhost:3001";
